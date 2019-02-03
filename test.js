98fc3235c57e07d8ec9b70698ba978552d1e8c83
irenerosaria <39551204+irenerosaria@users.noreply.github.com>
2019-02-03 20:57:07 +0100

    revisione
    revisione

--- test.js
+++ test.js
@@ -4,15 +4,16 @@ describe('testiamo il compito', function() {
 
 	it('testiamo getList',function(){
 		ex.reset();
-		assert.equal(ex.getList().length,0);
+        ex.addToDo("spolverare", "corridoio",true,"mamma");
+		assert.equal(ex.getList().length,1);
     })
     it('testiamo findListById',function(){
         ex.reset();
         ex.addToDo("spolverare", "corridoio",true,"mamma");
         ex.addToDo("lavare", "corridoio",false,"papà");
-        assert.equal(ex.findListById(0), ex.getList()[0]);
+        assert.equal(ex.findListById(0)[0], ex.getList()[0]);
     })
-	 it("testiamo addToDo e reset",function(){
+	 it("testiamo addToDo, reset e deleteById",function(){
         ex.reset();
         ex.addToDo("spolverare", "corridoio",true,"mamma");
         assert.equal(ex.getList().length, 1);
@@ -20,14 +21,14 @@ describe('testiamo il compito', function() {
         ex.addToDo("lavare", "corridoio",false,"papà");
         assert.equal(ex.getList().length, 2);
         assert.equal(ex.getList()[1].id, 1);
+        ex.addToDo("spolverare", "camera",true,"zio");
         ex.deleteById(0);
-        ex.addToDo("spolverare", "cucina",true,"mamma");
         assert.equal(ex.getList().length, 2);
         assert.equal(ex.getList()[1].id, 2);
 
     } )
 
-	 it('testiamo il findListByAssigned',function(){
+	 it('testiamo il findListByAssigned & findListById',function(){
 		ex.reset();
         ex.addToDo("spolverare", "corridoio",true,"mamma");
         ex.addToDo("lavare", "corridoio",false,"papà");
@@ -39,6 +40,7 @@ describe('testiamo il compito', function() {
         	assignedTo:"papà" ,
         	id:1
     	}
+        assert.deepEqual(ex.findListById(1)[0],element);
 		assert.deepEqual(ex.findListByAssigned("papà")[0], element);
 	})
 it('testiamo il changeBool',function(){
@@ -47,18 +49,15 @@ it('testiamo il changeBool',function(){
         ex.addToDo("lavare", "corridoio",false,"papà");
         assert.equal(ex.getList()[0].completed, true);
         assert.equal(ex.getList()[1].completed, false);
-        ex.changeBool(0,true);
         ex.changeBool(1,true);
-        assert.equal(ex.getList()[0].completed, true);
         assert.equal(ex.getList()[1].completed, true);
 	})
 
 it('testiamo il changeDescription',function(){
         ex.reset();
         ex.addToDo("spolverare", "corridoio",true,"mamma");
-        assert.equal(ex.getList()[0].description, "corridoio");
         ex.changeDescription(0,"scale");
-        assert.equal(ex.getList()[0].description, "scale");
+        assert.equal(ex.findListById(0)[0].description, "scale");
        
     })
 
